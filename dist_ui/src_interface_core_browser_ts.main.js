4a9433122f041ed7740de0254c031c6e95f85208
zeankundev <zeanfender11@gmail.com>
2021-10-07 06:58:02 +0700

    update appid, new font, etc

--- dist_ui/src_interface_core_browser_ts.main.js
+++ dist_ui/src_interface_core_browser_ts.main.js
@@ -1,2 +1,12 @@
 /*! For license information please see src_interface_core_browser_ts.main.js.LICENSE.txt */
-(global.webpackChunksuside=global.webpackChunksuside||[]).push([["src_interface_core_browser_ts"],{"./src/interface/core/browser.ts":(__unused_webpack_module,__webpack_exports__,__webpack_require__)=>{"use strict";eval("__webpack_require__.r(__webpack_exports__);\n/*\n * Tweaks for Browser\n */\n// Prevent showing browser's context menu\nwindow.addEventListener('contextmenu', e => e.preventDefault());\n// Disable Brwoser's Ctrl+P shortcut\nwindow.addEventListener('keydown', e => {\n    if (e.ctrlKey && ['p', 's', 'o'].includes(e.key)) {\n        e.preventDefault();\n    }\n});\n\n\n\n//# sourceURL=webpack://suside/./src/interface/core/browser.ts?")}}]);
+;(global.webpackChunksuside = global.webpackChunksuside || []).push([
+	['src_interface_core_browser_ts'],
+	{
+		'./src/interface/core/browser.ts': (__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+			'use strict'
+			eval(
+				"__webpack_require__.r(__webpack_exports__);\n/*\r\n * Tweaks for Browser\r\n */\r\n// Prevent showing browser's context menu\r\nwindow.addEventListener('contextmenu', e => e.preventDefault());\r\n// Disable Brwoser's Ctrl+P shortcut\r\nwindow.addEventListener('keydown', e => {\r\n    if (e.ctrlKey && ['p', 's', 'o'].includes(e.key)) {\r\n        e.preventDefault();\r\n    }\r\n});\r\n\r\n\n\n//# sourceURL=webpack://suside/./src/interface/core/browser.ts?",
+			)
+		},
+	},
+])
